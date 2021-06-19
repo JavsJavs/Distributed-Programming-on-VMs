c0d4dd4b16b593c1d2ab42fb46ecdc905bf13d71
JavsJavs <javiborrellweber@gmail.com>
2021-06-19 09:48:30 +0000

    Deleted redundant file, formatted

--- Practica1/multMatrix/main_multMatrix.cpp
+++ Practica1/multMatrix/main_multMatrix.cpp
@@ -3,17 +3,14 @@
 #include <iostream>
 #include "multmatrix.h"
 
-
-
 void freeMatrix(matrix_t* m){
     delete[] m->data;
     delete[] m;
-
 }
+
 int main()
 {
     multMatrix* mmatrix=new multMatrix();
-
 
     matrix_t* m1= mmatrix->createRandMatrix(5,5);
     matrix_t* m2= mmatrix->createIdentity(5,5);
@@ -23,13 +20,10 @@ int main()
     matrix_t* mres2=mmatrix->multMatrices(m1,m3);
     mmatrix->writeMatrix(mres2,"resultado2.txt");
 
-
-
     freeMatrix(m1);
     freeMatrix(m2);
     freeMatrix(mres);
     freeMatrix(m3);
     freeMatrix(mres2);
     delete mmatrix;
-
 }
